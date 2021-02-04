5f4a07f7cc96a37e313a44cebb4b2d5a4f7ff333
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-04 03:05:54 +0000

    Fix: Fix  puts  executable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main() {
-  puts("Programming is like building a multilingual puzzle\n");
-  return 0;     
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);     
 }
