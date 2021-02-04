8f39a353d34caac888164d6086a35634f2b19c49
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-04 04:24:17 +0000

    Feat: Create  quote   executable

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints a quote in the standard error
+ *
+ * Return: 1 if success.
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+);
+return (1);
+}
+
+
+
