8c28d712de853a0d1b17fe708dfe9b9945521b76
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-04 23:16:49 +0000

    Create print_alphabet executable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+	return (0);
+}
