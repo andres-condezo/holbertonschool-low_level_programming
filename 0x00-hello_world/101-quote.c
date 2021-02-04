66e2587350f09c957292aece1efabd772c4eec25
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-04 04:55:38 +0000

    Fix: Fix  quote   executable

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr
 );
 return (1);
 }
