8947789d4670943e19c543e0687be164b1724d52
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-04 21:14:13 +0000

    Fix: Fix positive_or_negative executable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,20 @@ int main(void)
 /* your code goes there */
 
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
+if (n > 0)
+{
+	printf("%d is positive\n", n);
 }
 else
 {
-		printf("%d is zero\n", n);
+if (n == 0)
+{
+	printf("%d is zero\n", n);
+}
+else
+{
+	printf("%d is negative\n", n);
+}
 }
 	return (0);
 }
