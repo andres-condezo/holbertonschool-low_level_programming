8938685d608f6abc3d7f77a366e08c99b0a411ee
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-05 04:06:28 +0000

    Fix  print_comb  executable

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,12 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		putchar(i);
+
 		if (i != '9')
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
-		}
-		else
-		{
-			putchar(i);
 		}
 	}
 		putchar('\n');
