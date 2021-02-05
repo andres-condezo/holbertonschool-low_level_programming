1d1cd7793ab5171669ec6571d6232aa27924c3f6
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-05 01:23:39 +0000

    Create print_numbers  executable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print last digit
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+int i
+for (i = 0; i <= 9; i++)
+{
+	putchar(i);
+}
+	putchar("\n");
+	return (0);
+}
