df3165467b1c3b3873b1043a7cb1325d2c679522
RobertoCondezo <ra.condezomonge@gmail.com>
2021-02-05 00:39:50 +0000

    Fix  executable

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-			putchar(ch);
-	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-			putchar(ch);
-	}
-		putchar('\n');
-		return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
 }
