4f206aeb7acfa6ac74afbbc43fb5cf94f5141592
adrs <ra.condezomonge@gmail.com>
2021-02-09 15:31:34 -0500

    Feat: Create jack-bauer function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.. 
+ *
+ * Return: Void
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 59; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar(10);
+		}
+	}
+}
