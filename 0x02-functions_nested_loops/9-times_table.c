185578347b8106504ddfd9ebcf53fbaac9194863
adrs <ra.condezomonge@gmail.com>
2021-02-09 17:09:26 -0500

    Fix: Fix times table  function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,51 +6,34 @@
  */
 void times_table(void)
 {
-
 	int fac1, fac2;
 	int prod, prodD, prodU;
 
 	for (fac1 = 0; fac1 <= 9; fac1++)
 	{
-		for (fac2 = 0; fac2 <= 9; fac2++)
+		_putchar('0');
+		for (fac2 = 1; fac2 <= 9; fac2++)
 		{
 			prod = fac1 * fac2;
+			prodD = prod / 10;
+			prodU = prod % 10;
 
 			if (prod > 9)
 			{
-				prodD = prod / 10;
-				prodU = prod % 10;
-
-				if (fac2 != 9)
-				{
-					_putchar(prodD + '0');
-					_putchar(prodU + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(prodD + '0');
-					_putchar(prodU + '0');
-					_putchar(10);
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prodD + '0');
+				_putchar(prodU + '0');
 			}
 			else
 			{
-				if (fac2 != 9)
-				{
-					_putchar(prod + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(prod + '0');
-					_putchar(10);
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
 			}
 		}
+		_putchar(10);
 	}
 }
