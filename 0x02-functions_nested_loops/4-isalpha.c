7c63fdd1a9f6ddae566d06d943597baf672afca0
adrs <ra.condezomonge@gmail.com>
2021-02-09 13:13:30 -0500

    Fix: fix _isalpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _isalpha - Checks a if a lleter is alpha..
+ * _isalpha - Checks a if a letter is alpha.
  *  @c: The char to be checked.
  *
  * Return: Returns 1  if c is a letter, lowercase or uppercase,
@@ -10,10 +10,11 @@ int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		if (c >= 97 && c <= 122)
-		{
-			return (1);
-		}
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
 	}
 	else
 	{
