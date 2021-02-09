2c29c2e0855b5df101b7c81240922912ff891892
adrs <ra.condezomonge@gmail.com>
2021-02-09 10:55:47 -0500

    Feat: Create print alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,15 @@
+#include "Holberton.h"
+/**
+ * print_alphabet - Prints alphabet in lower case
+ *
+ * Return: Void
+ */
 void print_alphabet(void)
 {
-
+  int i;
+  for (i = 'a'; i <= 'z'; i++)
+  {
+  _putchar(i);
+  }
+  _putchar('\n');
 }
