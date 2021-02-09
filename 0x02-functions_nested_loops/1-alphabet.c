33c64ffea67b86130b4dca55700dfa66fd1a1935
adrs <ra.condezomonge@gmail.com>
2021-02-09 10:59:42 -0500

    Fix: Fix print alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
  */
 void print_alphabet(void)
 {
-  int i;
-  for (i = 'a'; i <= 'z'; i++)
-  {
-  _putchar(i);
-  }
-  _putchar('\n');
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
