0452e67b02951bd2e7dccf9f3f23d44867f89466
adrs <ra.condezomonge@gmail.com>
2021-02-09 23:13:06 -0500

    Fix natural executable

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 /**
- * print_alphabet - Prints alphabet in lower case
+ * main - Prints the sum of all the multiples of 3 or 5
+ * below 1024 (excluded), followed by a new line.
  *
- * Return: Void
+ * Return: 0 success
  */
 int main(void)
 {
 	int i;
 	int sum;
 
-	for(i = 1; i < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
-		if((i % 3) == 0|| (i % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum = sum + i;
 		}
 	}
+
+	printf("%d\n", sum);
 
 
 	return (0);
