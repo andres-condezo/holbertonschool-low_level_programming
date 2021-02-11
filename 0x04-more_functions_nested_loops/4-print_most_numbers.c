38e806c66e31e6865537cd24a15d364f2bb2ed98
adrs <ra.condezomonge@gmail.com>
2021-02-11 15:49:20 -0500

    Create print_most_numbers  function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - the numbers, from 0 to 9, Do not print 2 and 4.
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar(10);
+
+}
