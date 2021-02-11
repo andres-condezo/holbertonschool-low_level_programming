5112b6646c7d98795e3706e908e0505a3c25969c
adrs <ra.condezomonge@gmail.com>
2021-02-11 15:31:33 -0500

    Create mul  function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - Multiplies two integers
+ * @a: First factor.
+ * @b: Second factor.
+ *
+ * Return: Void
+ */
+int mul(int a, int b)
+{
+int prod = a * b;
+
+return (prod);
+}
