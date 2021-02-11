a8ace50eeecab4797d16e98db1439bbc5619eaa5
adrs <ra.condezomonge@gmail.com>
2021-02-11 16:49:52 -0500

    Fix print_lines  function

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,17 @@ void print_line(int n)
 int i;
 char underscore = '_';
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		if (n > 0)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar(underscore);
+
 		}
+		_putchar(10);
 	}
-	_putchar(10);
+	else
+	{
+		_putchar(10);
+	}
 }
