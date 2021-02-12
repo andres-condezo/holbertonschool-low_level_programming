0a84eafc24d4df097c1c3002fde18325bfa6a1c7
adrs <ra.condezomonge@gmail.com>
2021-02-11 20:39:59 -0500

    Create print_trianglee

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * print_triangle - Prints a triangle, followed by a new line.
+ * @size; Is the size of the triangle.
+ *
+ * Return: Void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+	char hash = '#';
+	char space = ' ';
+
+	if(size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > i; j--)
+			{
+				_putchar(space);
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar(hash);
+			}
+		_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
