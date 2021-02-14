1209124f6f964672b19d78b61680dc2963d954d3
adrs <ra.condezomonge@gmail.com>
2021-02-14 14:43:15 -0500

    Fix times_table executable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,53 @@
 #include "holberton.h"
+
+/**
+ * print_one_digit - Prints the result to one digit
+ * @fac1: first factot.
+ * @fac2: second factot.
+ *
+ * Return: Void
+ */
+void print_one_digit(int fac1, int fac2)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar((fac1 * fac2) + '0');
+}
+
+/**
+ * print_two_digit - Prints the result to two digits
+ * @fac1: first factot.
+ * @fac2: second factot.
+ *
+ * Return: Void
+ */
+void print_two_digit(int fac1, int fac2)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(((fac1 * fac2) / 10)  + '0');
+	_putchar(((fac1 * fac2) % 10) + '0');
+}
+
+/**
+ * print_three_digit - Prints the result to three digits
+ * @fac1: first factot.
+ * @fac2: second factot.
+ *
+ * Return: Void
+ */
+void print_three_digit(int fac1, int fac2)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(((fac1 * fac2) / 100) + '0');
+	_putchar((((fac1 * fac2) % 100) / 10) + '0');
+	_putchar(((fac1 * fac2) % 10) + '0');
+}
+
 /**
  * print_times_table - Prints
  * the n times table, starting with 0.
@@ -9,9 +58,8 @@
 void print_times_table(int n)
 {
 	int fac1, fac2;
-	int prodC, prodD, prodU;
 
-	if (!(n == 0 || n > 15))
+	if (!(n < 0 || n > 15))
 	{
 		for (fac1 = 0; fac1 <= n; fac1++)
 		{
@@ -20,30 +68,26 @@ void print_times_table(int n)
 			{
 				if ((fac1 * fac2) > 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(prodC = ((fac1 * fac2) / 100) + '0');
-					_putchar(prodD = ((((fac1 * fac2) % 100) - prodU) / 10) + '0');
-					_putchar(prodU = ((fac1 * fac2) % 10) + '0');
+					print_three_digit(fac1, fac2);
 				}
 				else if ((fac1 * fac2) > 9 && (fac1 * fac2) <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(prodD = ((fac1 * fac2) / 10)  + '0');
-					_putchar(prodU = ((fac1 * fac2) % 10) + '0');
+					print_two_digit(fac1, fac2);
+				}
+				else if ((fac1 * fac2) >= 0 && (fac1 * fac2) <= 9)
+				{
+					print_one_digit(fac1, fac2);
 				}
 				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((fac1 * fac2) + '0');
+					break;
 				}
 			}
 			_putchar(10);
 		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
 	}
 }
