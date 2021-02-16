b2d56ec4251712269044d4227305501d7c2d646e
adrs <ra.condezomonge@gmail.com>
2021-02-16 16:41:22 -0500

    Init

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints alphabet in lower case
+ *
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+
+
+_putchar(10);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,1 +1,2 @@
 void reset_to_98(int *n);
+void reset_to_98(int *n);
