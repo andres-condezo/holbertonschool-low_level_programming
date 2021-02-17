e8073ea6d7c2ee4e59f7d0a88076fd39e6c4df49
adrs <ra.condezomonge@gmail.com>
2021-02-16 20:57:11 -0500

    Feat: Create rev_stringv function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string.
+ * @s: a pointer to an string.
+ *
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int len;
+	int j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i - 1;
+
+	while (len != 0)
+	{
+		temp = s[j];
+		s[j] = s[len];
+		s[len] = temp;
+
+		len--;
+		j++;
+	}
+}
