4a1a1028678cbe3bafdeed9963b1a353a504b97f
adrs <ra.condezomonge@gmail.com>
2021-02-16 23:12:14 -0500

    Fix: Fix puts2  function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
- * puts2 - Prints every other character of a string, starting with the first character. 
+ * puts2 - Prints every other character
+ * of a string, starting with the first character.
  * @str: a pointer to an int
  *
  * Return: Void
