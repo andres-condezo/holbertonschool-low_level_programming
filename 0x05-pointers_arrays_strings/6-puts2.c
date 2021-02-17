4bdf7fb3a26aa0e1d0a7109d36caa4ddc1699572
adrs <ra.condezomonge@gmail.com>
2021-02-16 23:08:31 -0500

    Fix: Create puts2  function

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * puts2 - Prints every other character of a string, starting with the first character. 
+ * @str: a pointer to an int
+ *
+ * Return: Void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+
+	int len = i;
+	int j;
+
+	for (j = 0; j <= len; j++)
+	{
+		if ((j % 2) == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+
+}
