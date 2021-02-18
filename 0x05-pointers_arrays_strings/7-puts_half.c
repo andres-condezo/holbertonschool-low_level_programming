9575381c857617c799547bfbd1fa73fd2fd2cae0
adrs <ra.condezomonge@gmail.com>
2021-02-17 21:11:36 -0500

    Feat: Create put_half  function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - Prints half of a string.
+ * @str: a pointer to an int
+ *
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	int len = i - 1;
+
+	if (len % 2 == 0)
+	{
+		int mid_even = (len / 2) + 1;
+
+		while (mid_even <= len)
+		{
+			_putchar(str[mid_even]);
+			mid_even++;
+		}
+	}
+	else
+	{
+		int mid_odd = (len / 2) + 2;
+
+		while (mid_odd <= len)
+		{
+			_putchar(str[mid_odd]);
+			mid_odd++;
+		}
+	}
+
+	_putchar('\n');
+}
