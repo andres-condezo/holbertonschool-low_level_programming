cdc818c05edaf31f7089d08e891f67f89c3bcc7a
adrs <ra.condezomonge@gmail.com>
2021-02-18 18:17:39 -0500

    Fix: Fix Srtcmp  function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int index = 0;
 	int res;
 
-	while (s1[index] != s2[index] && s1[index] != '\0' && s2[index] != '\0')
+	while ((s1[index] == s2[index]) && (s1[index] != '\0') && (s2[index] != '\0'))
 	{
 		index++;
 	}
