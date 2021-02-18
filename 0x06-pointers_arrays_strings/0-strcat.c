418eec8a556e8f07ffaabd7b5a2df5cc551db8b1
adrs <ra.condezomonge@gmail.com>
2021-02-18 12:41:48 -0500

    Fix: Fix Srtcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,26 +11,31 @@ char *_strcat(char *dest, char *src)
 {
 
 	int i = 0;
-	int index;
-	int len;
-
-	char *s = dest;
-
+	int j = 0;
+	int index = 0;
+	int len, len2;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	len = i;
-
-	for (index = 0; src[index] != '\0'; index++)
+	while (src[j] != '\0')
 	{
-		len++;
-		dest[len] = src[index];
+		j++;
 	}
 
-	dest[index] = src[index];
 
-	return (s);
+	len = i;
+	len2 = j;
+
+
+	while (index <= len2)
+	{
+		dest[len] = src[index];
+		index++;
+		len++;
+	}
+
+	return (dest);
 }
