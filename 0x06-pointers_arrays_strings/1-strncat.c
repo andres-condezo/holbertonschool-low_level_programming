3d789b9aed10158a2417a4f536b7a30e2685de2c
adrs <ra.condezomonge@gmail.com>
2021-02-18 13:26:34 -0500

    Fix: fix Srtncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,31 +12,39 @@ char *_strncat(char *dest, char *src, int n)
 {
 
 	int i = 0;
-	/* int j = 0; */
+	int j = 0;
 	int index = 0;
 	int len;
-	/* int len2; */
+	int len2;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	/* while (src[j] != '\0') */
-	/* { */
-	/* j++; */
-	/* } */
+	while (src[j] != '\0')
+	{
+		j++;
+	}
 
 
 	len = i;
-	/* len2 = j; */
+	len2 = j;
 
 
-	while (index <= n)
+	while (index <= len2)
 	{
-		dest[len] = src[index];
-		len++;
-		index++;
+		if (n > 0)
+		{
+			dest[len] = src[index];
+			len++;
+			index++;
+			n--;
+		}
+		else
+		{
+			break;
+		}
 	}
 
 	return (dest);
