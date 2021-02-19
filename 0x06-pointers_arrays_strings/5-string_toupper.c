8dc541a6ee59820013b24b0b952f1e5e0e424efe
adrs <ra.condezomonge@gmail.com>
2021-02-18 19:31:56 -0500

    Feat: Craete string_toupper  function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: An array of chars.
+ *
+ * Return: void.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+
+		i++;
+	}
+
+	return (s);
+
+}
