54d0df59a068811997cfd5daae26bcd545a11fce
adrs <ra.condezomonge@gmail.com>
2021-02-22 00:43:14 -0500

    Fix: Fix print_number function

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,26 @@
+
+#include "holberton.h"
+/**
+ * print_number - Prints an integer.
+ * @n: integer to be printed.
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+
+	unsigned int temp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if ((temp/10) > 10)
+	{
+		print_number(temp/10);
+	}
+
+	_putchar((temp % 10) + '0');
+}
