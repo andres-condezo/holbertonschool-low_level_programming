0298b8e23688f6471c6a439bd79c0272033ca081
adrs <ra.condezomonge@gmail.com>
2021-02-22 20:12:38 -0500

    Fix: Fix memcpy function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
 	return (dest);
