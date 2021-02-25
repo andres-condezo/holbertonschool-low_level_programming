7a9682a3e5f2256a4e9ca38748d9f86e7784c836
adrs <ra.condezomonge@gmail.com>
2021-02-24 23:35:40 -0500

    Feat: Create print_rev_recursion  function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Printsa string in reverse.
+ * @s: String to be printed.
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
