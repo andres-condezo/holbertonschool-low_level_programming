e736eb90cb6b3f35ffbfb61f6151bd5a332bd9ac
adrs <ra.condezomonge@gmail.com>
2021-03-04 03:01:26 -0500

    Feat: Create libholberton.a library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,4 +1,21 @@
+#include "holberton.h"
+/**
+ * _isupper - checks for uppercase character.
+ * @c: Character to be checked.
+ *
+ * Return: 0 success.
+ */
 int _isupper(int c)
 {
-	return (c);
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+return (0);
 }
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,10 @@
+#include "holberton.h"
+/**
+ * _atoi - checks for uppercase character.
+ * @s: Character to be checked.
+ *
+ * Return: 0 success.
+ */
 int _atoi(char *s)
 {
 	s[1] = 'a';
