0484b442c883315f9685458c5247d1982bddb867
adrs <ra.condezomonge@gmail.com>
2021-03-04 14:21:54 -0500

    Feat: Create args  executable file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: contains the number of arguments passed to the program..
+ * @argv: a one-dimensional array of strings.
+ *
+ * Return: 0 Success.
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+	argv = argv;
+
+	return (0);
+}
