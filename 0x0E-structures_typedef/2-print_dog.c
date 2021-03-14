681e8274573e6efd4de0aba5799a9b38840b9c5a
adrs <ra.condezomonge@gmail.com>
2021-03-14 12:15:20 -0500

    Feat: Create print_dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,31 +9,33 @@
 
 void print_dog(struct dog *d)
 {
-    if (d != NULL)
-    {
-        if (d->name != NULL)
-        {
-            printf("Name: %s\n", d->name);
-        }
-        else
-        {
-            printf("Name: (nil)\n");
-        }
-        if (d->age != '\0')
-        {
-            printf("Age: %f\n", d->age);
-        }
-        else
-        {
-            printf("Age: (nil)\n");
-        }
-        if (d->owner != NULL)
-        {
-            printf("Owner: %s\n", d->owner);
-        }
-        else
-        {
-            printf("Owner: (nil)\n");
-        }
-    }
+if (d != NULL)
+{
+if (d->name != NULL)
+{
+printf("Name: %s\n", d->name);
+}
+else
+{
+printf("Name: (nil)\n");
+}
+
+if (d->age)
+{
+printf("Age: %f\n", d->age);
+}
+else
+{
+printf("Age: (nil)\n");
+}
+
+if (d->owner != NULL)
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+{
+printf("Owner: (nil)\n");
+}
+}
 }
