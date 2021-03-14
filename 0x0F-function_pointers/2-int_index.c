53976a817b641592aa3634ee42d672cb18d2dec3
adrs <ra.condezomonge@gmail.com>
2021-03-14 14:18:03 -0500

    Feat: Create int_index function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+/**
+ * int_index - Search for an integer.
+ * @array: Array to be scanned.
+ * @size: Size of the array.
+ * @cmp: Pointer to the function you need to use.
+ *
+ * Return: Void.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array && size && cmp)
+{
+if (size <= 0)
+{
+return (-1);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+}
+}
+
+return (-1);
+}
