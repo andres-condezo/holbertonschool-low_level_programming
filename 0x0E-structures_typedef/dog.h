9f775d35fdb53543da9525b312aa9d3fed4c12fe
adrs <ra.condezomonge@gmail.com>
2021-03-14 05:48:27 -0500

    Feat: Create dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,20 @@
-#ifndef _0_object_like_macro_h
-#define _0_object_like_macro_h
-#define SIZE 1024
+#ifndef _0_DOG_h
+#define _0_DOG_h
+
+/**
+ * struct dog - dog data.
+ * @a: Dog name.
+ * @b: Dog age.
+ * @c: Dog owner.
+ *
+ * Description:  An struct for save dog information.
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 #endif
