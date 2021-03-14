f9a57c6172e60131feb8832358a98329514f8f83
adrs <ra.condezomonge@gmail.com>
2021-03-14 11:26:10 -0500

    Feat: Create init_dog function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "dog.h"
 /**
  * init_dog - Initialize a variable of type struct dog.
  * @d: name of new instance of dog structur.
@@ -8,9 +10,13 @@
  * Return: Void.
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-
-
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
 }
