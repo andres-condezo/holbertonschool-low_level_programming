7cdc1e063356782946bcf9c5aba84d0f3167ae4f
adrs <ra.condezomonge@gmail.com>
2021-03-14 12:35:38 -0500

    Feat: Create print_dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@
 
 void print_dog(struct dog *d)
 {
-if (d != NULL)
+if (d)
 {
-if (d->name != NULL)
+if (d->name)
 {
 printf("Name: %s\n", d->name);
 }
@@ -29,7 +29,7 @@ else
 printf("Age: (nil)\n");
 }
 
-if (d->owner != NULL)
+if (d->owner)
 {
 printf("Owner: %s\n", d->owner);
 }
