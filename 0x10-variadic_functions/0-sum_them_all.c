17da65ac20eeed4501659eeef3575d27822dc902
adrs <ra.condezomonge@gmail.com>
2021-03-15 03:54:54 -0500

    Feat: Create sum_them_all functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
+va_list ap;
+unsigned int i;
+int sum = 0;
+
 if (n == 0)
 {
 return (0);
 }
-va_list ap;
-unsigned int i;
-int sum = 0;
 
 va_start(ap, n);
 
