ef489854873a2f90fcc04f43800ac021bb71cd45
adrs <ra.condezomonge@gmail.com>
2021-03-15 03:44:49 -0500

    Feat: Create sum_them_all functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,26 +3,31 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-
+/**
+ * sum_them_all - Prints a sum.
+ * @n: number of parameters.
+ *
+ * Return: sum.
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
 
-    if(n == 0)
-    {
-        return (0);
-    }
-    va_list ap;
-    unsigned int i, sum;
+if (n == 0)
+{
+return (0);
+}
+va_list ap;
+unsigned int i, sum;
 
-    va_start (ap, n);
+va_start(ap, n);
 
-    sum = 0;
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg (ap, int);
-    }
+sum = 0;
+for (i = 0; i < n; i++)
+{
+sum += va_arg(ap, int);
+}
 
-        va_end (ap);
-        return sum;
+va_end(ap);
+return (sum);
 }
