213a65a8a68e9666d2f23b914da98360050d3a50
adrs <ra.condezomonge@gmail.com>
2021-03-15 03:00:01 -0500

    style: change style to op_gunc functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,7 +11,7 @@
 
 int op_add(int a, int b)
 {
-    return (a + b);
+return (a + b);
 }
 
 /**
@@ -24,7 +24,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-    return (a - b);
+return (a - b);
 }
 
 /**
@@ -37,7 +37,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
 
 /**
@@ -50,16 +50,16 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 
-    }
-    else
-    {
-        return (a / b);
-    }
+}
+else
+{
+return (a / b);
+}
 }
 
 /**
@@ -72,14 +72,14 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 
-    }
-    else
-    {
-        return (a % b);
-    }
+}
+else
+{
+return (a % b);
+}
 }
