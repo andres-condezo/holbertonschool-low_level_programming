c3684dc8da40698af40d419bcb2f4d2b4158d87d
adrs <ra.condezomonge@gmail.com>
2021-03-15 02:54:11 -0500

    Feat: Create main functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,8 +12,38 @@
 
 int main(int argc, char *argv[])
 {
-    int res;
+    int num1, num2, res;
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
+    int (*f)(int, int);
+
+
+    if(argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    switch (*(argv[2]))
+    {
+    case '+':
+            break;
+    case '-':
+            break;
+    case '*':
+            break;
+    case '/':
+            break;
+    case '%':
+            break;
+    default :
+        printf("Error\n");
+        exit(99);
+    }
+
+    f = get_op_func(argv[2]);
+    res = (*f)(num1, num2);
     printf("%d\n", res);
     return (0);
 }
