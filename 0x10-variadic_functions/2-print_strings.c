398634d40b5506aaf32b9fcf5c027aea023cd471
adrs <ra.condezomonge@gmail.com>
2021-03-16 04:36:32 -0500

    Feat: Create print_strings functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		char *string = va_arg(parameters, char *);
 
-		if ( string == NULL)
+		if (string == NULL)
 		{
 			printf("(nil)");
 		}
