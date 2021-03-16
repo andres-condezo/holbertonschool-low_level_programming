cd0f301488bbd41c4d47988797fa5f42a5c75241
adrs <ra.condezomonge@gmail.com>
2021-03-16 03:30:20 -0500

    Feat: Create print_numbers functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,31 +4,31 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - Prints a sum.
+ * print_numbers - Prints numbers.
+ * @separator: String to be printed between numbers.
  * @n: number of parameters.
  *
- * Return: sum.
+ * Return: void.
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list parameters;
-    unsigned int i;
+	va_list parameters;
+	unsigned int i;
 
 
-    va_start(parameters, n);
+	va_start(parameters, n);
 
-    for (i = 0; i < n; i++)
-    {
-        
-        printf("%d", va_arg(parameters, int));
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(parameters, int));
 
-        if (separator != NULL && i != (n - 1))
-        {
-            printf("%s", separator);
-        }
-    }
+		if (separator != NULL && i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
 
-    printf("\n");
-    va_end(parameters);
+	printf("\n");
+	va_end(parameters);
 }
