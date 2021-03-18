18a61b9d0653266882ec5dd2f46cb675d67f770d
adrs <ra.condezomonge@gmail.com>
2021-03-18 14:23:18 -0500

    Feat: create lists.h header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,9 +6,21 @@
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
-struct Node {
-    int data;
-    struct Node* next;
-};
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 #endif
