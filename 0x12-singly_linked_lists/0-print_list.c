dad38423187666f7a78b9e997e84ef52a5037ea8
adrs <ra.condezomonge@gmail.com>
2021-03-18 14:14:55 -0500

    Feat: create print_list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * sum_them_all - Prints a sum.
+ * @n: number of parameters.
+ *
+ * Return: sum.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+return (0);
+}
