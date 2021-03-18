f9cbae5c21397ff1bebadcd3331e3efff5377315
adrs <ra.condezomonge@gmail.com>
2021-03-18 14:09:49 -0500

    Docs(README.md): Add a project description

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FNCTIONS
+#define VARIADIC_FNCTIONS
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+#endif
