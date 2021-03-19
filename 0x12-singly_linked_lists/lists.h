af8a6c905658077d439c570c9d34d99d98dc5a07
adrs <ra.condezomonge@gmail.com>
2021-03-18 21:53:19 -0500

    Feat: Add print_list prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,9 +2,7 @@
 #define VARIADIC_FNCTIONS
 #include <stdio.h>
 #include <stdlib.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
+#include <stdarg.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +20,9 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
