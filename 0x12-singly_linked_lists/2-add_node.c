1b914c4f793e85a36d89e0554cbd3cb5fadc9616
adrs <ra.condezomonge@gmail.com>
2021-03-19 00:06:50 -0500

    Feat: Create add_node function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
-/**
- * _strlen - returns the length of a string.
- * @s: a pointer to a char.
- *
- * Return: The length of char *s.
- */
-int _strlen(const char *s)
-{
-
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
+int _strlen(const char *s);
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
@@ -56,4 +39,24 @@ list_t *add_node(list_t **head, const char *str)
 	*head = temp;
 
 	return (*head);
+}
+
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: a pointer to a char.
+ *
+ * Return: The length of char *s.
+ */
+int _strlen(const char *s)
+{
+
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
