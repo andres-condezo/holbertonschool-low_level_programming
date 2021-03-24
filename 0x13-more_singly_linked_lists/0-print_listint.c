29af5266495d507dca5601b21b5beb9eb691c961
adrs <ra.condezomonge@gmail.com>
2021-03-23 23:45:49 -0500

    Feat: Create print_listint function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a list_t list.
+ * @h: pointer to a list_t.
+ *
+ * Return: nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	size_t nodes = 0;
+
+	if (temp == NULL)
+	{
+		return (0);
+	}
+
+	while (temp != NULL)
+	{
+		if (temp->n == '\0')
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d\n", temp->n);
+		}
+		nodes++;
+		temp = temp->next;
+	}
+
+	return (nodes);
+}
