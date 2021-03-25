3898d05d4f566aafb5a27faf20c42cab0b814aae
adrs <ra.condezomonge@gmail.com>
2021-03-25 12:32:31 -0500

    Init: Init binary_to_uint function

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 chars.
+ *
+ * Return: the converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	return (0);
+}
