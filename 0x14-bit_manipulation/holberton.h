0ff6bbdd7caac9cd1de9de5506e6ff22253ecc59
adrs <ra.condezomonge@gmail.com>
2021-03-25 12:24:44 -0500

    Feat: Create holberton header

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
