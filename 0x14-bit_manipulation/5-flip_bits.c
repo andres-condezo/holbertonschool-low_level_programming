86f5ab8bbfcee3e03f87e2330655131911b9397b
adrs <ra.condezomonge@gmail.com>
2021-03-26 01:17:41 -0500

    Feat: Create flip_bit function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+
+/**
+ * flip_bits - Returns the number of bits you would need to
+ * flip to get from one number to another..
+ * @n: Number to be changed.
+ * @n: Number to desidered.
+ *
+ * Return: number of bits you would need to flip .
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	unsigned long int temp = n ^ m;
+	unsigned long int count = 0;
+
+	while (temp != 0)
+	{
+		count++;
+		temp &= (temp-1);
+        }
+
+	return (count);
+
+}
