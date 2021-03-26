40659f84a18db572ca73c7bb82ef66d42db4dc9e
adrs <ra.condezomonge@gmail.com>
2021-03-25 21:53:20 -0500

    Feat: Create get_bit function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: number to be searched.
+ * @index: the index, starting from 0 of the bit you want toet.
+ *
+ * Return: The value of the bit at index index or -1 if an error occured.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int res, mask;
+
+	mask = 1 << index;
+	res = mask & n;
+
+	return (res >> index);
+}
