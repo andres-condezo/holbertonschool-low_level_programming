1f602e8166530ff39111149500adac9a9c006def
adrs <ra.condezomonge@gmail.com>
2021-04-01 02:49:02 -0500

    Feat: Init read_textfile function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * read_textfile -  Reads a text file and prints it to the POSIX standard output.
+ * @filename: array of chars to be readed.
+ * @size_t: size of the array.
+ *
+ * Return: The actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+
+	return (0);
+}
+
