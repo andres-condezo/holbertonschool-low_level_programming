72c8c5afcc29ff6ea1416415d52584196b037052
adrs <ra.condezomonge@gmail.com>
2021-04-01 03:34:16 -0500

    Feat: Featt:add validators to my  read_textfile function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * read_textfile -  Reads a text file and prints it to the POSIX standard output.
+ * read_textfile -  Reads a text
+ * file and prints it to the POSIX standard output.
  * @filename: array of chars to be readed.
- * @size_t: size of the array.
+ * @letters: size of the array.
  *
  * Return: The actual number of letters it could read and print.
  */
@@ -11,25 +12,36 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int open_fd, read_fd, write_fd;
-	char * buf = malloc(sizeof(char) * letters);
+	char *buf = malloc(sizeof(char) * letters);
 
-	open_fd = open(filename, O_RDONLY);
-
-	if(open_fd == -1)
+	if (buf == NULL)
 	{
 		return (0);
 	}
 
-	read_fd = read (open_fd, buf, letters);
 
-	if(read_fd == -1)
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	open_fd = open(filename, O_RDONLY);
+
+	if (open_fd == -1)
+	{
+		return (0);
+	}
+
+	read_fd = read(open_fd, buf, letters);
+
+	if (read_fd == -1)
 	{
 		return (0);
 	}
 
 	write_fd = write(STDOUT_FILENO, buf, read_fd);
 
-	if(write_fd == -1)
+	if (write_fd == -1)
 	{
 		return (0);
 	}
