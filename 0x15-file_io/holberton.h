5c8b4ec2b160b7dbd1ad2b90c26f95f2c71f77f4
adrs <ra.condezomonge@gmail.com>
2021-04-01 02:22:32 -0500

    Feat: Add read_textfile prototype to my header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,10 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
-#include <ays/types.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
