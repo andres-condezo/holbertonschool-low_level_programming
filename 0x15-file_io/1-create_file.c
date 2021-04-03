6196711d271e0e75f7aef94468fb89813fb05aac
adrs <ra.condezomonge@gmail.com>
2021-04-03 04:10:14 -0500

    Feat: Create create_file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	open_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	open_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (open_fd == -1)
 	{
