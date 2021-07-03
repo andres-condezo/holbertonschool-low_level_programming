423b3a920fac9a13afbe4e0a3790c632fb89fef4
adrs condezo <ra.condezomonge@gmail.com>
2021-07-02 23:35:38 -0500

    Feat(5-island_perimeter.py): Create island_perimeter file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Island_perimeter module"""
+
+
+def island_perimeter(grid):
+    """That returns the perimeter of the island described in grid"""
+
+    count = 0
+
+    for row in grid:
+        for column in row:
+            if column == 1:
+                count += 1
+
+    perimeter = 2 * (count + 1)
+    return perimeter
