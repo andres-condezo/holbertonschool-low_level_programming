0e6b0a6b921bc81fbc029145d2e7ed56db7de9c4
adrs condezo <ra.condezomonge@gmail.com>
2021-07-04 13:46:13 -0500

    Feat(10-check_cycle.c): Add check_cycle function

---
+++ 0x00-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * check_cycle - Function in C that checks if a linked list has a cycle in it.
+ *
+ * @list: linked list to be checked
+ *
+ * Return: 0 if is a singly linked list
+ *		1 if is a circle linked list
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		if (fast == slow)
+		{
+			return (1);
+		}
+		slow = slow->next;
+	}
+
+	return (0);
+}
