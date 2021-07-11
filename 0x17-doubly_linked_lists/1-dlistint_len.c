4cd1253e81afcd6e1be1b8233ce6121dbe3e16b7
adrs condezo <ra.condezomonge@gmail.com>
2021-07-11 11:54:55 -0500

    Feat(1-dlistint_len.c):[Add 1-dlistint_len function]

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ *
+ * @h: A pointer to the list
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+
+	int nodes = 0;
+	const dlistint_t *tmp = h;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		nodes += 1;
+	}
+
+	return (nodes);
+}
