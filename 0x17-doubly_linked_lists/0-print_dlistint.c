4762fc45ebd50bf328690f27e1aec18a01390330
adrs condezo <ra.condezomonge@gmail.com>
2021-07-11 11:51:24 -0500

    Feat(0-print_Dlistint.c):[Add description to 0-print_dlistint function]

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,23 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ *
+ * @h: A pointer to the list
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
+
+
 	int nodes = 0;
 	const dlistint_t *tmp = h;
 
 	while (tmp)
 	{
-		printf("%d\n",tmp->n);
+		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 		nodes += 1;
 	}
