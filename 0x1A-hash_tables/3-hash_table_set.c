a707fb4f18e8d7f704cac2ffc5a5defab53d68c5
adrs condezo <ra.condezomonge@gmail.com>
2021-07-23 04:08:44 -0500

    Feat(3-hash_table_set.c):[Add hash_table_set script]

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	hash_node_t *temp;
+	unsigned long int index;
+	unsigned long int hash;
+
+	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0')
+	{
+		return (0);
+	}
+
+	hash = hash_djb2((const unsigned char *)key);
+	index = hash % ht->size;
+
+	for(temp = ht->array[index]; temp != NULL; temp = temp->next)
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+	new_node = malloc(sizeof(*new_node));
+	if(new_node == NULL)
+	{
+		return (0);
+	}
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
