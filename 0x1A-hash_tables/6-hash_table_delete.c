982abdfa6263cac8a08e41731aba523cbb958ee7
adrs condezo <ra.condezomonge@gmail.com>
2021-07-23 05:01:44 -0500

    Feat(6-gash_table_delete.c):[add hash_table_delete script]

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,37 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - a function that deletes a hash table.
- * @ht: s the hash table
- *
- * Return: Void
- */
+ * hash_table_delete - Deletes hash table
+ * @ht: Pointer to the hash table
+*/
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_table_t **array;
-	hash_table_t *temp;
-	hash_table_t *prev;
+	hash_node_t **array, *curr, *prev;
 
 	if (ht == NULL || ht->array == NULL)
-	{
 		return;
-	}
 
 	array = ht->array;
 	for (i = 0; i < ht->size; i++)
 	{
-		temp = array[i];
-		while (temp != NULL)
+		curr = array[i];
+		while (curr != NULL)
 		{
-			prev = temp;
-			temp = temp->next;
+			prev = curr;
+			curr = curr->next;
 			prev->next = NULL;
 			free(prev->key);
 			free(prev->value);
 			free(prev);
 		}
-		free(array);
-		free(ht);
+		array[i] = NULL;
 	}
+	free(array);
+	free(ht);
 }
