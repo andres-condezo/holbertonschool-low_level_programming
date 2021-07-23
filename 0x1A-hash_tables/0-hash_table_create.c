04bd789296507e15326a5289ee8e84a1d4549467
adrs condezo <ra.condezomonge@gmail.com>
2021-07-23 03:14:06 -0500

    Feat(0-hash_tacle_create.c):[Add hach_table_create script]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,6 +4,7 @@
  * hash_table_create - A function that creates a hash table.
  * @size: is the size of the array
  *
+ * Return: a pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -15,10 +16,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(*hash_table));
 	hash_table->array = malloc(sizeof(*(hash_table->array)) * size);
 
-	if(hash_table->array == NULL)
+	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return(NULL);
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
